f3ccb5ad172ac517c43a34dcde115601998b32b9
mohamadlounnas <mohamadlounnas@gmail.com>
2024-04-25 17:59:15 +0100

    Fix URL bug in main.tsx: Update host initialization logic and update script src in index.html to use latest version of index.js

--- src/main.tsx
+++ src/main.tsx
@@ -147,8 +147,8 @@ export const initApp = async (host: string) => {
 
 // Initialize the app with the current store
 var host = (new URL(
-"http://elkhalwi.asynx.store"
-//"http://asynx.khfif.shop"
+  window.location.href.includes("localhost") ? "http://elkhalwi.asynx.store" : window.location.href
+  //"http://asynx.khfif.shop"
 )).host
 initApp(host)
 
