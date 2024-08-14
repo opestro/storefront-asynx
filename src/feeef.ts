6627353ed8ec7cb97d79a7feafbeec3286823a5b
mohamadlounnas <mohamadlounnas@gmail.com>
2024-08-14 05:34:40 +0100

    chore: Update API key in feeef.ts

--- src/feeef.ts
+++ src/feeef.ts
@@ -1,7 +1,7 @@
 import { FeeeF } from "feeef/src/feeef/feeef";
 
 export const ff = new FeeeF({
-    apiKey: "API_KEY",
+    apiKey: "c43Yfd3bgolijJU3b3bx095vlfTrvnL94baZrd1",
     baseURL: "https://apis.feeef.net/api/v1",
     // cache: 2000,
     // baseURL: "http://localhost:3333/api/v1",
