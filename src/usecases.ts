047e802f771adb16b72bdc6c8956572f6762fb94
mohamadlounnas <mohamadlounnas@gmail.com>
2024-08-29 10:58:05 +0100

    refactor: Improve lazy loading behavior and variant selection logic in product page

--- src/usecases.ts
+++ src/usecases.ts
@@ -1,17 +1,19 @@
 
 import { ProductEntity, StoreEntity } from "feeef/src/core/core";
 import {ff} from "./feeef"
-
 var _stores: Record<string, StoreEntity> = {};
 var _products: Record<string, ProductEntity> = {};
 var _storeProducts: Record<string, ProductEntity[]> = {};
 
 export async function getStore(host: string): Promise<StoreEntity> {
     if (_stores[host]) return _stores[host];
-    let subdomain = host.split(".")[0];
+
+    // if contains XXX.feeef.shop use slug, else domain.name
+    const isFeeefShop = host.includes(".feeef.shop");
+
     var store = await ff.stores.find({
-        id: subdomain,
-        by: "slug",
+        id: isFeeefShop? host.split(".")[0] : host,
+        by: isFeeefShop? "slug" : "domain.name",
     });
     _stores[host] = store;
     return store;
