2ea27e079b3c43afbaa75e08c5ab2ed096d67e8d
opestro <mahdiharzallah21@gmail.com>
2024-09-03 21:34:36 +0100

    Fix gTag v1

--- src/App.tsx
+++ src/App.tsx
@@ -122,11 +122,11 @@ function Home() {
   gtag('config', 'G-PHHZC0B2SR');`,
           }}
         />
-        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        <script dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-T8JWQPMC');</script>
+})(window,document,'script','dataLayer','GTM-T8JWQPMC');`}}></script>
       </head>
 
       <SuperSEO
